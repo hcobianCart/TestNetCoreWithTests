a2ea7d74b2839032eee4333f0874d2558c751e0c
hcobianCart <c-hans.cobian@cart.com>
2021-08-26 14:47:47 -0500

    otros cambios

---
+++ src/testAPI/testAPI/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace testAPI.Controllers
+{
+
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        public TestController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
+
+        // GET: api/<Test>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            if (!Env.IsProduction())
+            {
+                return Ok(
+                    new
+                    {
+                        IsDevelopment = Env.IsDevelopment(),
+                        EnvironmentName = Env.EnvironmentName,
+                        audience = Configuration["Auth0Management:audience"],
+                        client_id = Configuration["Auth0Management:client_id"],
+                        Domain = Configuration["Auth0Management:Domain"]
+                    }
+                    );
+            }
+            else
+                return NotFound();
+        }
+
+    }
+}
