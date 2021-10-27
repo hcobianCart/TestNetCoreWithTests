504a1007a8809dfcd88e1fc11b3c90511b20d40f
hcobianCart <c-hans.cobian@cart.com>
2021-10-27 15:23:17 -0500

    mas cambios

--- src/testAPI/testAPI/Startup.cs
+++ src/testAPI/testAPI/Startup.cs
@@ -39,81 +39,7 @@ namespace testAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "testAPI", Version = "v1" });
             });
 
-            
-
-            services.AddHealthChecks()
-                .AddCheck("appsettings", () =>
-                {
-                    Dictionary<string, object> options = new Dictionary<string, object>();
-                    var ErrorStr = string.Empty;
-                    if (string.IsNullOrEmpty(Configuration["Auth0Management:audience"]))
-                        options.Add("Auth0Management__audience", "");
-                    if (string.IsNullOrEmpty(Configuration["Auth0Management:Domain"]))
-                        options.Add("Auth0Management__Domain", "");
-                    if (string.IsNullOrEmpty(Configuration["Auth0Management:client_id"]))
-                        options.Add("Auth0Management__client_id", "");
-                    if (string.IsNullOrEmpty(Configuration["Auth0Management:client_secret"]))
-                        options.Add("Auth0Management__client_secret", "");
-                    if (string.IsNullOrEmpty(Configuration["Auth0:Domain"]))
-                        options.Add("Auth0__Domain", "");
-                    if (string.IsNullOrEmpty(Configuration["Auth0:ApiIdentifier"]))
-                        options.Add("Auth0__ApiIdentifier", "");
-                    if (string.IsNullOrEmpty(Configuration["CartDatabaseSettings:UsersCollectionName"]))
-                        options.Add("CartDatabaseSettings__UsersCollectionName", "");
-                    if (string.IsNullOrEmpty(Configuration["CartDatabaseSettings:ConnectionString"]))
-                        options.Add("CartDatabaseSettings__ConnectionString", "");
-                    if (string.IsNullOrEmpty(Configuration["CartDatabaseSettings:DatabaseName"]))
-                        options.Add("CartDatabaseSettings__DatabaseName", "");
-
-                    if (options.Count > 0)
-                        return HealthCheckResult.Degraded($"Missing Configuration", null, options);
-                    else
-                        return HealthCheckResult.Healthy("appsettings file hasn't missing or empty values");
-                }, new[] { "Configuration" });
-            try
-            {
-                if (!string.IsNullOrEmpty(Configuration["CartDatabaseSettings:ConnectionString"]))
-                {
-                    services.AddSingleton(new MongoDbHealthCheck(Configuration["CartDatabaseSettings:ConnectionString"]));
-                    services.AddHealthChecks().AddMongoDb(
-                        mongodbConnectionString: Configuration["CartDatabaseSettings:ConnectionString"],
-                        name: "Ping MongoDB",
-                        failureStatus: HealthStatus.Unhealthy,
-                        timeout: new TimeSpan(0, 0, 2),
-                        tags: new[] { "DB" });
-                }
-            }
-            catch (Exception ex)
-            {
-                services.AddHealthChecks().AddCheck("Ping MongoDB", () => HealthCheckResult.Unhealthy(" Error CartDatabaseSettings__ConnectionString ", ex), tags: new[] { "DB" });
-            }
-            try
-            {
-                if (!string.IsNullOrEmpty(Configuration["Auth0:Domain"]))
-                    services.AddHealthChecks().AddUrlGroup(
-                        uri: new Uri(Configuration["Auth0:Domain"]),
-                        name: "Ping Auth0__Domain",
-                        failureStatus: HealthStatus.Unhealthy,
-                        timeout: new TimeSpan(0, 0, 2),
-                        tags: new[] { "Endpoints" });
-            }
-            catch (Exception ex) {
-                services.AddHealthChecks().AddCheck("Ping Auth0__Domain", () => HealthCheckResult.Unhealthy("Error Auth0__Domain ", ex), tags: new[] { "Endpoints" });
-            }
-            try
-            {
-                if (!string.IsNullOrEmpty(Configuration["Auth0Management:Domain"]))
-                    services.AddHealthChecks().AddUrlGroup(
-                        uri: new Uri(Configuration["Auth0Management:Domain"]),
-                        name: "Ping Auth0Management__Domain",
-                        failureStatus: HealthStatus.Unhealthy,
-                        timeout: new TimeSpan(0, 0, 2),
-                        tags: new[] { "Endpoints" });
-            }
-            catch (Exception ex)
-            {
-                services.AddHealthChecks().AddCheck("Ping Auth0Management__Domain", () => HealthCheckResult.Unhealthy("Error Auth0Management__Domain ", ex), tags: new[] { "Endpoints" });
-            }
+            services.AddHealthChecks().AddCheck("config", () => HealthCheckResult.Healthy(), tags: new[] { "test" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -133,25 +59,6 @@ namespace testAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHealthChecks("/quickhealth", new HealthCheckOptions()
-                {
-                    Predicate = _ => false
-                });
-                endpoints.MapHealthChecks("/health/Configuration", new HealthCheckOptions()
-                {
-                    Predicate = reg => reg.Tags.Contains("Configuration"),
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
-                endpoints.MapHealthChecks("/health/DB", new HealthCheckOptions()
-                {
-                    Predicate = reg => reg.Tags.Contains("DB"),
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
-                endpoints.MapHealthChecks("/health/Endpoints", new HealthCheckOptions()
-                {
-                    Predicate = reg => reg.Tags.Contains("Endpoints"),
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
                 endpoints.MapHealthChecks("/health", new HealthCheckOptions()
                 {
                     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
